cfa61d5908a20361ff9176618ce25e1e507a9316
Lucia V <luciamvila@gmail.com>
2025-02-11 14:58:11 -0500

    First draft of active learning code

---
+++ Chp 5/code/ch5activeLearning.R
@@ -0,0 +1,79 @@
+library(tidyverse)
+library(ggplot2)
+source("ch5activeLearningFunctions.R")
+source("ch5activeLearningShinyApp.R")
+
+# ---- 1. Creating the population ----
+# Create a set of 250 thousand entries, where 88% of them are "support"
+# and 12% are "not".
+pop_size <- 250000
+pop_proportion <- 0.88
+population <- create_population(N=pop_size, p=pop_proportion)
+head(population)
+tail(population)
+sum(population == "support") / pop_size  # Should be 0.88
+# TODO: chart to visualize population?
+
+
+# ---- 2. Sampling from the population ----
+samples_10 <- sample(population, size=10)
+samples_100 <- sample(population, size=100)
+samples_1000 <- sample(population, size=1000)
+samples_10000 <- sample(population, size=10000)
+head(samples_10000)
+# TODO: chart to visualize some samples?
+
+
+# ---- 3. Computing p-hat ----
+# Count the number that are "support", then divide by the sample size. 
+
+# Q: What happens to p-hat as the sample size increases?
+sum(samples_10 == "support") / 10
+sum(samples_100 == "support") / 100
+sum(samples_1000 == "support") / 1000
+sum(samples_10000 == "support") / 10000
+# TODO: Rshiny table?
+
+
+# ---- 4. Central Limit Theorem ---- 
+# Take many samples from the population (simulation) to construct a dataset. 
+# Then observe the result of the Central Limit Theorem; namely: 
+# as the number of samples grows large, the sample distribution of p-hat can be 
+# approximated by a Normal distribution.
+
+# Q: How do you think the shape of the sampling distribution changes with 
+# different simulation sizes? 
+# Q: What determines where the center or mean of the sampling distribution will
+# fall?  
+K <- 1000  # Simulation size
+simulation_100 <- replicate(K, sample_get_phat_fn(population, n=100))
+hist(simulation_100, xlim=range(0, 1))
+
+ggplot(data=data.frame(simulation_100), aes(x=simulation_100)) +
+  geom_vline(aes(xintercept=pop_proportion), color="red") +
+  geom_histogram(bins=100, alpha=0.5, color=4, fill="white") +
+  labs(title="Histogram of p-hat values from experiment with 1000x100 samples",
+       x="Sample proportion", y="Frequency") +
+  xlim(c(0,1))
+
+# Q: Where do the parameters for the Normal distribution approximating p-hat 
+# come from?
+# TODO: fix
+#SE_phat100 <- sqrt((pop_proportion*(1-pop_proportion))/100)
+#normal_dist <- rnorm(1000, mean=pop_proportion, sd=SE_phat100)
+
+#ggplot(data=data.frame(c(simulation_100, normal_dist)), aes(x=simulation_100)) +
+#  geom_density(lwd=1, color=4,fill=4, alpha=0.25)
+
+#lines(data.frame(normal_dist), color=2)
+# TODO: move to helper function and just grab the graphs in this file?
+
+# 5. ---- Repeat the experiment ---- 
+# Now construct datasets from samples of different sizes and repeat the above. 
+
+# Q: What happens to the distributions as sample size increases?
+
+# Run Rshiny app
+shinyApp(ui = ui, server = server)
+
+# TODO: add sample & normal densities to app
---
+++ Chp 5/code/ch5activeLearningFunctions.R
@@ -0,0 +1,11 @@
+create_population <- function(N=10000, p=0.5) {
+  pop <- c(rep("support", p*N), rep("not", (1-p)*N))
+  return(pop)
+}
+
+sample_get_phat_fn <- function(population, n) {
+  sampled_entries <- sample(population, size = n)
+  phat <- sum(sampled_entries == "support") / n
+  return(phat)
+}
+
