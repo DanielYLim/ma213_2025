4213d6a09b38ede82bd9654c4e7aed53e0e5ade9
Emily Stephen <emilyps14@gmail.com>
2025-08-05 13:58:35 -0400

    Updating todos and R demo for Lecture 4

--- Week2/Lecture4/Lecture4_demo.R
+++ Week2/Lecture4/Lecture4_demo.R
@@ -7,14 +7,45 @@ setwd(dirname(getSourceEditorContext()$path))  # set working directory
 
 set.seed(42)
 
-# ---- 1. Load and visualize data ----
+# ---- 1. Load data ----
 data(airquality)
 
-ggplot(data=airquality, aes(x=Wind, y=Ozone)) +
+head(airquality)
+
+# Let's deliberately add an incorrect entry
+new_row <- data.frame(
+  Ozone = 50,      # example value
+  Solar.R = 200,   # example value
+  Wind = -5,       # outlier: negative wind speed
+  Temp = 80,       # example value
+  Month = 6,       # example value
+  Day = 15         # example value
+)
+airquality2 <- rbind(airquality, new_row) # add the row to the dataset
+tail(airquality2)
+
+# ---- 2. Visualize data
+
+ggplot(data=airquality2, aes(x=Wind, y=Ozone)) +
   geom_point()
+
+ggplot(data=airquality2, aes(x = Wind)) +
+  geom_histogram()
 
 # ---- 2. Boxplots ----
 
-ggplot(data=airquality, aes(x=Wind, y=Ozone)) +
-  geom_boxplot(outlier.color = "orange") +
-  geom_point(color="light blue")
+ggplot(data=airquality2, aes(y=Wind)) +
+  geom_boxplot(outlier.color = "orange")
+
+# Note: This is the code we used last time to plot the IQR:
+ggplot(data=airquality2, aes(x = Wind)) +
+  geom_histogram() +
+  geom_boxplot(aes(x = Wind),  # Place boxplot below x-axis
+               width = 1, 
+               outlier.shape = NA,
+               coef = 0,
+               position = position_nudge(y = -2)
+  ) 
+guides(fill = "none")
+
+
