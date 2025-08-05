c7faeddacd19837d7dbca7b8674b41dc2e176661
Emily Stephen <emilyps14@gmail.com>
2025-08-05 13:16:28 -0400

    Revisions to Lecture 3 demo

--- Week2/Lecture3/Lecture3_demo.R
+++ Week2/Lecture3/Lecture3_demo.R
@@ -1,22 +1,20 @@
 # ---- 0. Setup and load libraries, if any ----
 
 library(ggplot2)  # load the graphing library
+library(dplyr)    # load the data manipulation library
 
 if(!require("rstudioapi")) install.packages("rstudioapi")
 setwd(dirname(getSourceEditorContext()$path))  # set working directory
 
-set.seed(42)
 source("Lecture3_demoFunctions.R")
 
 # ---- 1. Generate data ----
 
-datasets <- generate_data1()
-
-data1 <- datasets$data1
+data1 <- generate_data1()
 head(data1)
-data2 <- datasets$data2
-data3 <- datasets$data3
-data4 <- datasets$data4
+
+# Count the number of rows for each dataset
+table(data1$dataset)
 
 
 # ---- 2. Plot histograms ----
@@ -25,57 +23,74 @@ data4 <- datasets$data4
 # i.e. are they uni-modal/bimodal/multimodal, or uniform? What are the mode(s)?
 # Are they skewed, left/right, or symmetric? Are there any outliers?
 
-ggplot(data=data1, aes(x=x1)) +
-  geom_histogram(bins=15, col='gray')
+ggplot(data=data1, aes(x=x, fill=as.factor(dataset))) +
+  geom_histogram() +
+  facet_wrap(~dataset, ncol=1, scales = "free_y") +
+  guides(fill = "none")
 
-ggplot(data=data2, aes(x=x2)) +
-  geom_histogram(bins=15, col='gray')
-
-ggplot(data=data3, aes(x=x3)) +
-  geom_histogram(bins=15, col='gray')
-
-ggplot(data=data4, aes(x=x4)) +
-  geom_histogram(bins=15, col='gray')
-
-
-# Let's look at one of the datasets more closely:
+# Let's look at the datasets more closely:
 # What is the mean here? What about the median? And the mode(s)?
 # If the mean and median are different, why do you think that is?
+data1_summary <- data1 %>%
+  group_by(dataset) %>%
+  summarise(
+    mean = mean(x),
+    median = median(x),
+  )
+print(data1_summary)
 
-ggplot(data=data2, aes(x=x2)) +
-  geom_histogram(bins=15, col='gray') +
-  geom_vline(xintercept=mean(data2$x2), col='red') +
-  geom_vline(xintercept=median(data2$x2), col='orange')
-
+ggplot(data=data1, aes(x=x, fill=as.factor(dataset))) +
+  geom_histogram() +
+  facet_wrap(~dataset, ncol=1, scales = "free_y") +
+  geom_vline(data = data1_summary, aes(xintercept = mean, col='mean')) +
+  geom_vline(data = data1_summary, aes(xintercept = median, col='median')) +
+  scale_color_manual(
+    name = "Statistic",
+    values = c(mean = "red", median = "blue"),
+    labels = c(mean = "Mean", median = "Median")
+  ) +
+  guides(fill = "none")
 
 # ---- 2. Comparing shapes of distributions and their outliers ----
 
-# Let's try changing the variance of our distribution and see how its 
-# shape changes:
-data5 <- generate_data2()
-head(data5)
+# Let's try looking at distributions with different variances:
+data2 <- generate_data2()
+head(data2)
 
-ggplot(data=data5, aes(x=x, fill=as.factor(variance))) +
-  geom_histogram(col='gray', alpha=0.6)
+table(data2$dataset)
 
-# We can also examine these data with boxplots:
-ggplot(data=data5, aes(x=x, color=as.factor(variance))) +
-  geom_boxplot()
+# Look at the distributions
+ggplot(data=data2, aes(x=x, fill=as.factor(dataset))) +
+  geom_histogram() +
+  facet_wrap(~dataset, ncol=1) +
+  guides(fill = "none")
 
-ggplot(data=data5, aes(x=x, fill=as.factor(variance))) +
-  geom_histogram(col='gray', alpha=0.6) +
-  geom_boxplot
+# Compute summary statistics for central tendency and variability
+data2_summary <- data2 %>%
+  group_by(dataset) %>%
+  summarise(
+    mean = round(mean(x),3),
+    variance = var(x),
+    std_dev = sd(x),
+    Q1 = quantile(x, 0.25),
+    median = median(x),
+    Q3 = quantile(x, 0.75),
+    IQR = IQR(x)
+  )
+print(data2_summary)
 
-# Now let's look at the highest-variance distribution more closely:
-data6 <- data5[data5$variance == 100,] 
-lower_sd <- mean(data6$x) - 2*sd(data6$x)
-upper_sd <- mean(data6$x) + 2*sd(data6$x)
 
-# Q: keep the boxplot in this histogram? feels a little crowded
-ggplot(data=data6, aes(x=x)) +
-  geom_histogram(col='gray', alpha=0.7)  +
-  geom_histogram(col='gray', alpha=0.6) +
-  geom_boxplot(outlier.alpha = 0) +  
-  geom_vline(xintercept=mean(data6$x), col='red') +
-  geom_vline(xintercept=lower_sd, col='orange') +
-  geom_vline(xintercept=upper_sd, col='orange')
+# Plot the distributions again, with lines to show mean+/-2SD
+plot <- histograms_plusminus2sd(data2)
+print(plot)
+
+# We can also look at the IQRs:
+plot2 <- histograms_plusIQR(data2)
+print(plot2)
+
+# What if the distributions are skewed?
+data3 <- generate_data3()
+
+print(histograms_plusminus2sd(data3))
+print(histograms_plusIQR(data3))
+
--- Week2/Lecture3/Lecture3_demoFunctions.R
+++ Week2/Lecture3/Lecture3_demoFunctions.R
@@ -1,24 +1,104 @@
 set.seed(42)
 
 generate_data1 <- function() {
-  x1 <- rchisq(65, 6)
-  x2 <- c(rchisq(22, 5.8), rnorm(40, 16.5, 2))
-  x3 <- c(rchisq(20, 3), rnorm(35, 12), rnorm(42, 18, 1.5))
-  x4 <- runif(100,0,20)
+  x1 <- rchisq(100, 3)
+  x2 <- 20-rchisq(100, 2)
+  x3 <- c(rchisq(60, 5.8), rnorm(40, 16.5, 2))
+  x4 <- c(rchisq(20, 3), rnorm(35, 12), rnorm(45, 18, 1.5))
+  x5 <- runif(100,0,20)
   
-  return(list("data1" = as.data.frame(x1), "data2" = as.data.frame(x2), 
-              "data3" = as.data.frame(x3), "data4" = as.data.frame(x4)))
+  values = c(x1, x2, x3, x4, x5)
+  datasets = c(rep("A", 100), rep("B", 100), rep("C", 100), 
+               rep("D", 100), rep("E", 100))
+  
+  data <- do.call(rbind, Map(data.frame, x=values, dataset=datasets))
+  return(data)
+}
+
+generate_data2 <- function() {
+  x1 <- rnorm(100, 0, 1)
+  x2 <- rnorm(100, 0, 5)
+  x3 <- rnorm(100, 0, 10)
+  
+  values = append(append(x1, x2), x3)
+  datasets = c(rep("F", 100), rep("G", 100), rep("H", 100))
+  
+  data <- do.call(rbind, Map(data.frame, x=values, dataset=datasets))
+  return(data)
+}
+
+generate_data3 <- function() {
+  x1 <- rchisq(100, 2)/2
+  x2 <- rchisq(100, 2)*2
+  x3 <- rchisq(100, 2)*5
+  
+  values = append(append(x1, x2), x3)
+  datasets = c(rep("I", 100), rep("J", 100), rep("K", 100))
+  
+  data <- do.call(rbind, Map(data.frame, x=values, dataset=datasets))
+  return(data)
 }
 
 
-generate_data2 <- function() {
-  x1 <- rnorm(100, 0, sd=sqrt(1.0))
-  x2 <- rnorm(100, 0, sd=sqrt(10.0))
-  x3 <- rnorm(100, 0, sd=sqrt(100.0))
+
+# ---- Plotting functions
+
+histograms_plusminus2sd <- function(df) {
+  library(dplyr)
+  library(ggplot2)
   
-  values = append(append(x1, x2), x3)
-  vars = c(rep(1.0, 100), rep(10.0, 100), rep(100.0, 100))
+  # Compute mean, sd, segment start/end
+  summary_df <- df %>%
+    group_by(dataset) %>%
+    summarise(
+      m = mean(x, na.rm = TRUE),
+      sd = sd(x, na.rm = TRUE)
+    ) %>%
+    mutate(
+      xstart = m - 2*sd,
+      xend = m + 2*sd
+    )
   
-  data <- do.call(rbind, Map(data.frame, x=values, variance=vars))
-  return(data)
+  # histogram with segment
+  p <- ggplot(df, aes(x = x, fill = as.factor(dataset))) +
+    geom_histogram() +
+    facet_wrap(~dataset, ncol = 1) +
+    geom_segment(
+      data = summary_df,
+      aes(x = xstart, xend = xend, y = -5, yend = -5),
+      color = "black",
+      linewidth = 1,
+      inherit.aes = FALSE
+    ) +
+    geom_point(
+      data = summary_df,
+      aes(x = m, y = -5),
+      color = "black",
+      shape = 4, # shape 4 is an "x"
+      size = 1,
+      stroke = 1,
+      inherit.aes = FALSE
+    ) +
+    guides(fill = "none")+
+    ylab("count")
+  
+  return(p)
+}
+
+
+histograms_plusIQR <- function(df) {
+  library(ggplot2)
+
+  p <- ggplot(df, aes(x = x, fill = as.factor(dataset))) +
+    geom_histogram() +
+    geom_boxplot(aes(group = as.factor(dataset)),  # Place boxplot below x-axis
+                 width = 5, 
+                 outlier.shape = NA,
+                 coef = 0,  # remove the whiskers
+                 position = position_nudge(y = -5)
+    ) +
+    facet_wrap(~dataset, ncol=1) +
+    guides(fill = "none")
+  
+  return(p)
 }
