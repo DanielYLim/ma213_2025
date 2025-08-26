0bd23221b3c0d53055ab6112a4a989ffb0a525c7
Emily Stephen <emilyps14@gmail.com>
2025-08-26 17:47:36 -0400

    Minor edits to Lecture 10 Demo 2

--- Week4/Lecture10/Lecture10_demo2.R
+++ Week4/Lecture10/Lecture10_demo2.R
@@ -8,26 +8,37 @@ setwd(dirname(getSourceEditorContext()$path))  # set working directory
 
 # ---- 1. Calculate standard normal distribution probabilities ----
 
+m = 3 # Mean 
+s = 2 # Standard Deviation 
+
 # Previously, we used dnorm() to generate the normal probability density.
+x <- seq(-5,11,0.1)  # xaxis values
+
+ggplot(data.frame(x), aes(x)) +
+  stat_function(fun=dnorm, args=list(mean=m, sd=s), color='steelblue')
+
 # Now, we can calculate probabilities using pnorm(), the distribution function
 # for the Normal. 
+# What is more likely (has a higher probability): X <= 8, or X <= 0.1?
 
-# Imagine we have a standard normal distribution (mean=0, sd=1). What is more
-# likely (has a higher probability): X <= 8, or X <= 0.1?
-
-pnorm(8)    # P(X <= 8)
-pnorm(0.1)  # P(X <= 0.1)
+pnorm(8, mean=m, sd=s)    # P(X <= 8)
+pnorm(0.1, mean=m, sd=s)  # P(X <= 0.1)
 
 # What else is likely (or unlikely)?
 
+# What is the pnorm function doing?
+ggplot(data.frame(x), aes(x)) +
+  stat_function(fun=pnorm, args=list(mean=m, sd=s), color='steelblue')
 
 # ---- 2. Calculate other normal distribution probabilities ----
 
 # Let's look at an extreme example:
-pnorm(-5, mean=10, sd=1)  # P(X <= -5) when X is tightly concentrated around +10
+m = 10
+s = 1
+pnorm(-5, mean=m, sd=s)  # P(X <= -5) when X is tightly concentrated around +10
 
 x <- seq(-10,20,0.1)
 ggplot(data.frame(x), aes(x)) +
-  stat_function(fun=dnorm, args=list(mean=10, sd=1), color='steelblue') +
+  stat_function(fun=dnorm, args=list(mean=m, sd=s), color='steelblue') +
   geom_vline(xintercept=-5, color='red')
 
