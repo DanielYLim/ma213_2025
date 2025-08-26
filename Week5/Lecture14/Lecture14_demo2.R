a38b7d2cef98cde50aae8bae69076ae85bfa5be0
Emily Stephen <emilyps14@gmail.com>
2025-08-26 19:18:01 -0400

    Lecture 14 r demo 2 as bar chart

--- Week5/Lecture14/Lecture14_demo2.R
+++ Week5/Lecture14/Lecture14_demo2.R
@@ -8,11 +8,11 @@ library(ggplot2)  # load the graphing library
 samples1 <- rpois(n=100, lambda=3)  # Sample with given parameters
 
 # Plot the same distribution
-ggplot(data.frame(x=c(0:100)), aes(x)) +
-  geom_point(aes(y=dpois(x, lambda=3)))  # dpois gives the density of the values in x
+ggplot(data.frame(x=c(0:40)), aes(x)) +
+  geom_col(aes(y=dpois(x, lambda=0.5)))  # dpois gives the density of the values in x
 
-ggplot(data.frame(x=c(0:100)), aes(x)) +
-  geom_point(aes(y=dpois(x, lambda=50)))
+ggplot(data.frame(x=c(0:40)), aes(x)) +
+  geom_col(aes(y=dpois(x, lambda=3)))
 
-ggplot(data.frame(x=c(0:100)), aes(x)) +
-  geom_point(aes(y=dpois(x, lambda=100)))
+ggplot(data.frame(x=c(0:40)), aes(x)) +
+  geom_col(aes(y=dpois(x, lambda=20)))
